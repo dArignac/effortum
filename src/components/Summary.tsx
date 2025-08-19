b0394c60d9447096e65af725ce119560650ed418
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-08-19 07:25:05 +0200

    fixes display of calendar and time list #24

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,9 +1,8 @@
-import { BarChart } from "@mantine/charts";
-import { Grid } from "@mantine/core";
+import { Box, Flex, SimpleGrid } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useEffortumStore } from "../store";
 import { filterTasksByDateRange } from "../utils/filters";
 import { formatDuration, getDuration } from "../utils/time";
@@ -47,35 +46,29 @@ export function Summary() {
   ).sort((a, b) => a.project.localeCompare(b.project));
 
   return (
-    <Grid>
-      <Grid.Col span={12} style={{ display: "flex", justifyContent: "center" }}>
-        <DatePicker
-          type="range"
-          allowSingleDateInRange
-          value={selectedDateRange}
-          onChange={(value) => setSelectedDateRange(value || [null, null])}
-          size="xs"
-        />
-      </Grid.Col>
-      <Grid.Col span={12}>
-        {data.length === 0 ? (
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            No data available
-          </div>
-        ) : (
-          <BarChart
-            h={300}
-            w={350}
-            data={data}
-            dataKey="project"
-            orientation="vertical"
-            barProps={{ barSize: 25 }}
-            withBarValueLabel
-            valueFormatter={(value) => formatDuration(value)}
-            series={[{ name: "time", color: "blue.6" }]}
-          />
-        )}
-      </Grid.Col>
-    </Grid>
+    <Flex
+      mih={50}
+      gap="sm"
+      justify="flex-start"
+      align="flex-start"
+      direction="column"
+      wrap="wrap"
+    >
+      <DatePicker
+        type="range"
+        allowSingleDateInRange
+        value={selectedDateRange}
+        onChange={(value) => setSelectedDateRange(value || [null, null])}
+        size="xs"
+      />
+      <SimpleGrid cols={2} spacing="xs" verticalSpacing="xs" mt={5}>
+        {data.map((task) => (
+          <Fragment key={task.project}>
+            <Box component="strong">{task.project}:</Box>
+            <Box>{formatDuration(task.time)}</Box>
+          </Fragment>
+        ))}
+      </SimpleGrid>
+    </Flex>
   );
 }
