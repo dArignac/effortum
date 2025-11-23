7e1d467fb10a2c33f729e1e0ceebf097fc1eca77
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-11-23 08:56:02 +0100

    chore: moves version, adds dummy export button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,32 @@
 import { Summary } from "@/components/Summary";
 import { TaskList } from "@/components/TaskList";
-import { Grid } from "@mantine/core";
+import { VERSION } from "@/version";
+import { Center, Container, Grid, Text, Button } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ ssr: false, component: App });
 
 function App() {
   return (
-    <Grid overflow="hidden">
-      <Grid.Col span={8}>
-        <TaskList />
-      </Grid.Col>
-      <Grid.Col span={4}>
-        <Summary />
-      </Grid.Col>
-    </Grid>
+    <>
+      <Grid overflow="hidden">
+        <Grid.Col span={8}>
+          <TaskList />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Summary />
+        </Grid.Col>
+      </Grid>
+      <Container size="xs" mt={40}>
+        <Center>
+          <Text size="xs">Version: {VERSION} </Text>
+        </Center>
+        <Center mt={10}>
+          <Button component="a" size="compact-xs">
+            Export Data
+          </Button>
+        </Center>
+      </Container>
+    </>
   );
 }
