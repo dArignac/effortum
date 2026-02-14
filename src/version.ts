f71f517545c3d01c4c0b0a3be2e091b884946d1a
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2026-02-14 11:13:34 +0100

    fix: updates dependencies

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,8 @@
 // manual poor mans approach to versioning :D
-export const VERSION = "0.7.0";
+export const VERSION = "0.7.1";
 
 /**
+ * 0.7.1 Updates dependencies to latest versions
  * 0.7.0 Adds basic layout and navigation
  * 0.6.2 Updates packages to latest versions
  */
