11aa76ceaf4947893937468f0af00218f4b39529
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2026-02-15 12:58:32 +0100

    fix: improves task list layout

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,9 @@
 // manual poor mans approach to versioning :D
-export const VERSION = "0.8.0";
+export const VERSION = "0.8.1";
 
 /**
- * 0.8.0 Adds overtime handling
+ * 0.8.1 Improves the layout for longer project and task names
+ * 0.8.0 Adds overtime storage
  * 0.7.1 Updates dependencies to latest versions
  * 0.7.0 Adds basic layout and navigation
  * 0.6.2 Updates packages to latest versions
